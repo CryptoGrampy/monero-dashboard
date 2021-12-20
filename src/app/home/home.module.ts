6f2f738448c8ac9dc7d402abe943190722660bb8
CryptoGrampy <cryptogrampy@protonmail.com>
2021-12-20 15:09:32 -0600

    Fix build, disable browser open on ng serve command

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
-
+import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
